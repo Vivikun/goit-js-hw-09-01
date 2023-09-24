86c8d12571d6e477f65e9472c8445f65d2a6bfe3
ignacy <ignacpowala@gmail.com>
2023-09-24 14:24:03 +0200

    Summarty

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,41 @@
+import Notiflix from 'notiflix';
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
 }
+
+document.querySelector('form').addEventListener('submit', async e => {
+  e.preventDefault();
+  const firstDelay = parseInt(
+    document.querySelector('input[name="delay"]').value,
+    10
+  );
+  const step = parseInt(document.querySelector('input[name="step"]').value, 10);
+  const amount = parseInt(
+    document.querySelector('input[name="amount"]').value,
+    10
+  );
+
+  for (let i = 0; i < amount; i++) {
+    const position = i + 1;
+    const delay = firstDelay + i * step;
+
+    // Corrected 'then' to 'await createPromise'
+    await createPromise(position, delay)
+      .then(({ position, delay }) => {
+        Notiflix.Notify.success(`Fulfilled promise ${position} in ${delay} ms`);
+      })
+      .catch(({ position, delay }) => {
+        Notiflix.Notify.failure(`Rejected promise ${position} in ${delay} ms`);
+      });
+  }
+});
